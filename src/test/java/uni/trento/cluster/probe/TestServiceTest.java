26f7a24bf32ec90e3482cda35d23b774a3d05fb7
DTS1234 <adas.kazmierczak@gmail.com>
2023-05-30 22:19:04 +0200

    Add CI/CD

---
+++ src/test/java/uni/trento/cluster/probe/TestServiceTest.java
@@ -0,0 +1,13 @@
+package uni.trento.cluster.probe;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestServiceTest {
+
+    @Test
+    void name() {
+        Assertions.assertThat(2 + 2).isEqualTo(4);
+    }
+
+}
